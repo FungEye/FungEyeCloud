522cdff54d468acb9316687ce2b87e828b5a42da
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-03-23 10:53:55 +0100

    setup Java project
    - Included dependencies that I suppose we are going to use. We can always add/remove as we go.
    - Dependencies:
      - Spring Web
      - Spring WebSocket
      - Spring JPA (along with Postgresql driver)
      - Lombok

---
+++ src/main/java/fungeye/cloud/FungEyeCloudApplication.java
@@ -0,0 +1,13 @@
+package fungeye.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FungEyeCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FungEyeCloudApplication.class, args);
+    }
+
+}
