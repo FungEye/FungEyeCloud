1ec944d189c5e3eb9c3ab7266bba420fbbe5b96d
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-03-27 10:26:28 +0200

    Added repositories and fixed duplicate sql statement

---
+++ src/main/java/fungeye/cloud/persistence/repository/MushroomRepository.java
@@ -0,0 +1,7 @@
+package fungeye.cloud.persistence.repository;
+
+import fungeye.cloud.domain.enities.Mushroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MushroomRepository extends JpaRepository<Mushroom, Long> {
+}
