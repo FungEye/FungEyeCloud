7eadb370e5348f05f39769aa975d384c044f507d
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-05-01 22:27:22 +0200

    Added users and roles
    TG-88
    
    ADD:
    - User and role entities
    - Migration script for these
    - Repositories for User and Role
    - SecurityConfig.java (really simple for now - just enables security)
    - Added Spring Security Starter dependency to pom.xml

---
+++ src/main/java/fungeye/cloud/domain/enities/users/UserEntity.java
@@ -0,0 +1,32 @@
+package fungeye.cloud.domain.enities.users;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new LinkedHashSet<>();
+
+}
