8998c175a884e70bc23b7ab1b20dc47bae73bb54
chrfoyer <chrfoyer@gmail.com>
2023-05-06 13:31:36 +0200

    test: More mappers
    TG-97: Test classes for mappers of grow, measured conditions, and mushrooms

---
+++ src/test/java/fungeye/cloud/service/mappers/GrowMapperTest.java
@@ -0,0 +1,111 @@
+package fungeye.cloud.service.mappers;
+
+import fungeye.cloud.domain.dtos.GrowDto;
+import fungeye.cloud.domain.enities.Box;
+import fungeye.cloud.domain.enities.Grow;
+import fungeye.cloud.domain.enities.Mushroom;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GrowMapperTest {
+
+    @Test
+    public void testMapToGrowDto() {
+        // Arrange
+        Box box = new Box();
+        box.setId(1L);
+
+        Set<Mushroom> mushrooms = new HashSet<>();
+        mushrooms.add(new Mushroom());
+        mushrooms.add(new Mushroom());
+
+        LocalDate dateStarted = LocalDate.of(2023, 5, 1);
+
+        Grow grow = new Grow();
+        grow.setId(2L);
+        grow.setBox(box);
+        grow.setIsActive(true);
+        grow.setDateStarted(dateStarted);
+        grow.setDevelopmentStage("Early");
+        for (Mushroom mush:
+             mushrooms) {
+            mush.setGrow(grow);
+        }
+        grow.setMushrooms(mushrooms);
+
+
+        // Act
+        GrowDto dto = GrowMapper.mapToGrowDto(grow);
+
+
+
+        // Assert
+        assertNotNull(dto);
+        assertEquals(grow.getId(), dto.getId());
+        assertEquals(box.getId(), dto.getBoxId());
+        assertEquals(mushrooms.size(), dto.getMushroomDtoList().size());
+        assertEquals(grow.getIsActive(), dto.isActive());
+        assertEquals(dateStarted.getDayOfMonth(), dto.getDate().getDay());
+        assertEquals(dateStarted.getMonthValue(), dto.getDate().getMonth());
+        assertEquals(dateStarted.getYear(), dto.getDate().getYear());
+        assertEquals(grow.getDevelopmentStage(), dto.getStage());
+    }
+
+    @Test
+    public void testMapToGrowDtoList() {
+        // Arrange
+        Box box1 = new Box();
+        box1.setId(1L);
+        Box box2 = new Box();
+        box2.setId(2L);
+
+        Set<Mushroom> mushrooms = new HashSet<>();
+        mushrooms.add(new Mushroom());
+        mushrooms.add(new Mushroom());
+
+        LocalDate dateStarted = LocalDate.of(2023, 5, 1);
+
+        Grow grow1 = new Grow();
+        grow1.setId(2L);
+        grow1.setBox(box1);
+        grow1.setIsActive(true);
+        grow1.setDateStarted(dateStarted);
+        grow1.setDevelopmentStage("Early");
+        for (Mushroom mush:
+                mushrooms) {
+            mush.setGrow(grow1);
+        }
+        grow1.setMushrooms(mushrooms);
+
+        Grow grow2 = new Grow();
+        grow2.setId(2L);
+        grow2.setBox(box1);
+        grow2.setIsActive(true);
+        grow2.setDateStarted(dateStarted);
+        grow2.setDevelopmentStage("Early");
+        for (Mushroom mush:
+                mushrooms) {
+            mush.setGrow(grow2);
+        }
+        grow2.setMushrooms(mushrooms);
+
+        Set<Grow> grows = new HashSet<>();
+        grows.add(grow1);
+        grows.add(grow2);
+
+        // Act
+        List<GrowDto> dtos = GrowMapper.mapToGrowDtoList(grows);
+
+        // Assert
+        assertNotNull(dtos);
+        assertEquals(grows.size(), dtos.size());
+        assertEquals(grow2.getId(), dtos.get(0).getId());
+        assertEquals(grow2.getId(), dtos.get(1).getId());
+    }
+}
---
+++ src/test/java/fungeye/cloud/service/mappers/MushroomMapperTest.java
@@ -0,0 +1,89 @@
+package fungeye.cloud.service.mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import fungeye.cloud.domain.enities.Grow;
+import fungeye.cloud.domain.enities.Mushroom;
+import fungeye.cloud.domain.dtos.MushroomDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class MushroomMapperTest {
+
+    private static final Long MUSHROOM_ID = 1L;
+    private static final String MUSHROOM_NAME = "Shiitake";
+    private static final String MUSHROOM_DESC = "Large, dark-brown mushroom with an earthy flavor";
+    private static final Long GROW_ID = 2L;
+
+    private MushroomMapper mushroomMapper;
+
+    @Mock
+    private Mushroom mushroomMock;
+
+    @Mock
+    private Grow growMock;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mushroomMapper = new MushroomMapper();
+    }
+
+    @Test
+    public void testMapToMushroomDto() {
+        when(mushroomMock.getId()).thenReturn(MUSHROOM_ID);
+        when(mushroomMock.getName()).thenReturn(MUSHROOM_NAME);
+        when(mushroomMock.getDescription()).thenReturn(MUSHROOM_DESC);
+        when(mushroomMock.getGrow()).thenReturn(growMock);
+        when(growMock.getId()).thenReturn(GROW_ID);
+
+        MushroomDto mushroomDto = mushroomMapper.mapToMushroomDto(mushroomMock);
+
+        assertEquals(MUSHROOM_ID, mushroomDto.getId());
+        assertEquals(MUSHROOM_NAME, mushroomDto.getName());
+        assertEquals(MUSHROOM_DESC, mushroomDto.getDescription());
+        assertEquals(GROW_ID, mushroomDto.getGrowId());
+    }
+
+    @Test
+    public void testMapToMushroomDtoList() {
+        Set<Mushroom> mushroomSet = new HashSet<>();
+        mushroomSet.add(mushroomMock);
+
+        when(mushroomMock.getId()).thenReturn(MUSHROOM_ID);
+        when(mushroomMock.getName()).thenReturn(MUSHROOM_NAME);
+        when(mushroomMock.getDescription()).thenReturn(MUSHROOM_DESC);
+        when(mushroomMock.getGrow()).thenReturn(growMock);
+        when(growMock.getId()).thenReturn(GROW_ID);
+
+        List<MushroomDto> mushroomDtoList = mushroomMapper.mapToMushroomDtoList(mushroomSet);
+
+        assertEquals(1, mushroomDtoList.size());
+
+        MushroomDto mushroomDto = mushroomDtoList.get(0);
+
+        assertEquals(MUSHROOM_ID, mushroomDto.getId());
+        assertEquals(MUSHROOM_NAME, mushroomDto.getName());
+        assertEquals(MUSHROOM_DESC, mushroomDto.getDescription());
+        assertEquals(GROW_ID, mushroomDto.getGrowId());
+    }
+
+    @Test
+    public void testMapToMushroomDtoListEmpty() {
+        Set<Mushroom> mushroomSet = Collections.emptySet();
+
+        List<MushroomDto> mushroomDtoList = mushroomMapper.mapToMushroomDtoList(mushroomSet);
+
+        assertEquals(0, mushroomDtoList.size());
+    }
+
+}
