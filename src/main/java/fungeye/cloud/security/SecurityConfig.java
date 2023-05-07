21ae3eb4d355ccbc31e3f675a8e783bd4f9e6e9c
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-05-07 15:12:43 +0200

    cleanup
    Removed some unused import statements and comments

--- src/main/java/fungeye/cloud/security/SecurityConfig.java
+++ src/main/java/fungeye/cloud/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // TODO disable cors?? play around with this
+                .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(authEntryPoint)
                 .and()
--- src/main/java/fungeye/cloud/security/SecurityConstants.java
+++ src/main/java/fungeye/cloud/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package fungeye.cloud.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 900000;
+    public static final long JWT_EXPIRATION = 900000; // Token expires after 15 minutes?
     public static final String JWT_SECRET = "secret";
 }
