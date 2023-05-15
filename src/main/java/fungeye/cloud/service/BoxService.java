ec155f69b2632f87469bd18b39d62f9f9692b3a5
JusteBuu <90906175+JusteBuu@users.noreply.github.com>
2023-05-16 01:13:41 +0200

    GetAllBoxes By user id service and controller impl.
    The testing is not done yet

--- src/main/java/fungeye/cloud/service/BoxService.java
+++ src/main/java/fungeye/cloud/service/BoxService.java
@@ -4,8 +4,10 @@ import fungeye.cloud.domain.dtos.BoxDetailsDto;
 import fungeye.cloud.domain.dtos.BoxDto;
 import fungeye.cloud.domain.enities.Box;
 import fungeye.cloud.persistence.repository.BoxRepository;
+import fungeye.cloud.service.mappers.BoxMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static fungeye.cloud.service.mappers.BoxMapper.*;
@@ -28,9 +30,22 @@ public class BoxService {
         return mapToBoxDtoList(repository.findAll());
     }
 
-
     public BoxDetailsDto getById(Long id)
     {
         return mapToBoxDto(repository.findById(id).orElseThrow());
     }
+
+    public List<BoxDto> getAllByUserName(String userName) {
+
+        List<Box> boxes = repository.findBoxesByUserEntity_Username(userName);
+        List<BoxDto> dtoList = new ArrayList<>();
+
+        for (Box box : boxes)
+        {
+            dtoList.add(BoxMapper.mapToSimpleDto(box));
+        }
+        return dtoList;
+    }
+
+
 }
