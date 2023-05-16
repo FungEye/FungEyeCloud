b1afd99ca64112ab57039d8364d1f6203b34145e
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-05-16 14:30:16 +0200

    Cleaned up imports

--- src/main/java/fungeye/cloud/domain/dtos/HistoricalMeasurementDto.java
+++ src/main/java/fungeye/cloud/domain/dtos/HistoricalMeasurementDto.java
@@ -3,9 +3,7 @@ package fungeye.cloud.domain.dtos;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @Data
 public class HistoricalMeasurementDto implements Serializable {
--- src/main/java/fungeye/cloud/domain/dtos/SingleMeasurementDto.java
+++ src/main/java/fungeye/cloud/domain/dtos/SingleMeasurementDto.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Data
 public class SingleMeasurementDto implements Serializable {
