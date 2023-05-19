c582bf3ac0db89baede24b19a93fb00c9181a6ab
chrfoyer <chrfoyer@gmail.com>
2023-05-19 02:48:56 +0200

    fix: Include most recent develop branch
    I had forgotten to include tests I had written myself mere hours ago

--- src/test/java/fungeye/cloud/controllers/MushroomControllerTest.java
+++ src/test/java/fungeye/cloud/controllers/MushroomControllerTest.java
@@ -274,7 +274,7 @@ class MushroomControllerTest {
 
         IdealCondition idealCondition1 = new IdealCondition();
 
-        idealCondition1.setId(new IdealConditionId(mushroom3.getId(), "Spawn run"));
+        idealCondition1.setId(new IdealConditionId(mushroom3.getId(), "spawn run"));
         idealCondition1.setMushroom(mushroom3);
         idealCondition1.setTemperatureLow(20.0);
         idealCondition1.setTemperatureHigh(25.0);
