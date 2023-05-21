163858376500a7e4051e9a8f1527ee1cd2aa4632
chrfoyer <chrfoyer@gmail.com>
2023-05-21 22:18:58 +0200

    test: Using conditions for the mushroom creation
    Testing the new custom and default mushroom creation endpoints with the conditions. Additionally, there is a little more auth testing

--- src/test/java/fungeye/cloud/security/JwtAuthenticationFilterTest.java
+++ src/test/java/fungeye/cloud/security/JwtAuthenticationFilterTest.java
@@ -7,33 +7,99 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class JwtAuthenticationFilterTest {
     @Mock
-    private HttpServletRequest request;
+    private JwtGenerator tokenGenerator;
     @Mock
-    private HttpServletResponse response;
-    @Mock
-    private FilterChain filterchain;
-
-    private JwtAuthenticationFilter test;
+    private CustomUserDetailsService userDetailsService;
+    @InjectMocks
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @BeforeEach
-    void setUp() {
+    public void setup() {
         MockitoAnnotations.openMocks(this);
-        test = new JwtAuthenticationFilter();
     }
 
     @Test
-    void doFilterInternal() throws ServletException, IOException {
-    assertDoesNotThrow(() -> test.doFilterInternal(request, response, filterchain));
+    public void doFilterInternal_ValidToken_SuccessfullyAuthenticatesUser() throws ServletException, IOException {
+        // Arrange
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        FilterChain filterChain = mock(FilterChain.class);
+
+        String token = "validToken";
+        String username = "testuser";
+
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+        when(tokenGenerator.validateToken(token)).thenReturn(true);
+        when(tokenGenerator.getUsernameFromJwt(token)).thenReturn(username);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetailsService.loadUserByUsername(username)).thenReturn(userDetails);
+
+        // Act
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        // Assert
+        verify(userDetails, times(1)).getAuthorities();
+        verify(filterChain, times(1)).doFilter(request, response);
+        verify(request, times(1)).getHeader("Authorization");
+        verify(tokenGenerator, times(1)).validateToken(token);
+        verify(tokenGenerator, times(1)).getUsernameFromJwt(token);
+        verify(userDetailsService, times(1)).loadUserByUsername(username);
+    }
+
+    @Test
+    public void doFilterInternal_InvalidToken_DoesNotAuthenticateUser() throws ServletException, IOException {
+        // Arrange
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        FilterChain filterChain = mock(FilterChain.class);
+
+        String token = "invalidToken";
+
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+        when(tokenGenerator.validateToken(token)).thenReturn(false);
+
+        // Act
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        // Assert
+        verify(filterChain, times(1)).doFilter(request, response);
+        verify(request, times(1)).getHeader("Authorization");
+        verify(tokenGenerator, times(1)).validateToken(token);
+        verify(tokenGenerator, never()).getUsernameFromJwt(token);
+        verify(userDetailsService, never()).loadUserByUsername(anyString());
+    }
+
+    @Test
+    public void doFilterInternal_NoToken_DoesNotAuthenticateUser() throws ServletException, IOException {
+        // Arrange
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        FilterChain filterChain = mock(FilterChain.class);
+
+        when(request.getHeader("Authorization")).thenReturn(null);
+
+        // Act
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        // Assert
+        verify(filterChain, times(1)).doFilter(request, response);
+        verify(request, times(1)).getHeader("Authorization");
+        verify(tokenGenerator, never()).validateToken(anyString());
+        verify(tokenGenerator, never()).getUsernameFromJwt(anyString());
+        verify(userDetailsService, never()).loadUserByUsername(anyString());
     }
 }
