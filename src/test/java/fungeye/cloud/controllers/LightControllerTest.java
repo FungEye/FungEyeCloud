acab302523e55ade7ea10825167ba56beb2b9477
chrfoyer <chrfoyer@gmail.com>
2023-05-22 18:50:21 +0200

    test: toggle light endpoint
    TG-95: Testing for the controller and service in relation to toggling the light

---
+++ src/test/java/fungeye/cloud/controllers/LightControllerTest.java
@@ -0,0 +1,39 @@
+package fungeye.cloud.controllers;
+
+import fungeye.cloud.service.LightService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+class LightControllerTest {
+
+    @Mock
+    LightService lightService;
+
+    private LightController controller;
+
+    @BeforeEach
+    void setup() {
+        openMocks(this);
+        controller = new LightController(lightService);
+    }
+
+    @Test
+    void testToggleLight() {
+        int testBoxId = 2;
+
+        when(lightService.toggleLight(testBoxId)).thenReturn(true);
+
+        ResponseEntity<Boolean> response = controller.toggleLight(testBoxId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(true, response.getBody());
+    }
+}
