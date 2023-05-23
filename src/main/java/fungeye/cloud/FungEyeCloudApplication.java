37622f41fd4508f5b4bace92c39da32be79f5a7f
ChristianHougaardPedersen <90906202+ChristianHougaardPedersen@users.noreply.github.com>
2023-05-23 21:41:40 +0200

    Update FungEyeCloudApplication.java
    Added timezone so google cloud works properly

--- src/main/java/fungeye/cloud/FungEyeCloudApplication.java
+++ src/main/java/fungeye/cloud/FungEyeCloudApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class FungEyeCloudApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Copenhagen"));
         SpringApplication.run(FungEyeCloudApplication.class, args);
     }
 }
