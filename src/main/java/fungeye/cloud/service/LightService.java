ba3512ea171ddfee6724ac9c35ca8ac3ac648ea9
chrfoyer <chrfoyer@gmail.com>
2023-05-24 20:41:43 +0200

    feat: Show how the EUI would be used
    Makes a private helper method that shows how the EUI would be found using the box id

--- src/main/java/fungeye/cloud/service/LightService.java
+++ src/main/java/fungeye/cloud/service/LightService.java
@@ -1,8 +1,11 @@
 package fungeye.cloud.service;
 
+import fungeye.cloud.domain.enities.Box;
 import fungeye.cloud.persistence.repository.BoxRepository;
 import fungeye.cloud.websockets.HardwareTutorial;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LightService {
@@ -16,8 +19,9 @@ public class LightService {
     }
 
     public boolean toggleLight(int boxId) {
-        // Todo: Get the EUI using the boxId
-        // The EUI of the device is used, but will be taken from the box in the future
+        // When more actual devices are used in the system, their EUI will be taken from the box
+        // String eui = getEUIFromBoxId(boxId);
+
         String eui = "0004A30B00ED6757";
 
         // This payload is for toggling the light
@@ -26,5 +30,14 @@ public class LightService {
                 eui + "\",\"port\": 1,\"confirmed\" : true,\"data\": \"" + dataPayload + "\"}";
         hardwareTutorial.sendDownLink(jsonToSend);
         return true;
+    }
+    private String getEUIFromBoxId(int boxId) {
+        Optional<Box> box = boxRepository.findById((long) boxId);
+        if (box.isPresent()) {
+            return box.get().getEui();
+        }
+        else {
+            throw new IllegalArgumentException("No box was found with that id");
+        }
     }
 }
