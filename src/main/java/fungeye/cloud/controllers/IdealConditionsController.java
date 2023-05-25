fd1025a3a1e760dfce2219c36b2765873e256936
JusteBuu <90906175+JusteBuu@users.noreply.github.com>
2023-05-25 10:37:14 +0200

    Controller refractor

--- src/main/java/fungeye/cloud/controllers/IdealConditionsController.java
+++ src/main/java/fungeye/cloud/controllers/IdealConditionsController.java
@@ -19,15 +19,13 @@ public class IdealConditionsController {
     }
 
     @PostMapping("/ideal")
-    public ResponseEntity<IdealConditionDto> createIdealCondition(@RequestBody IdealConditionDto dto)
-    {
+    public ResponseEntity<IdealConditionDto> createIdealCondition(@RequestBody IdealConditionDto dto) {
         IdealConditionDto saved = service.createIdealCondition(dto);
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/ideal/{mushroomId}")
-    public ResponseEntity<List<IdealConditionDto>> getIdealConditionsByMushroomId(@PathVariable Long mushroomId)
-    {
+    public ResponseEntity<List<IdealConditionDto>> getIdealConditionsByMushroomId(@PathVariable Long mushroomId) {
         List<IdealConditionDto> found = service.getByMushroomId(mushroomId);
         return new ResponseEntity<>(found, HttpStatus.OK);
     }
